d4c73d3537743fa444ba1eaa13dcb036b1b14d4b
Jorge Hidalgo <jorge.hidalgo@gmail.com>
2017-10-22 19:08:24 +0200

    initial commit

---
+++ src/main/java/deors/demos/microservices/bookrecedgeservice/BookController.java
@@ -0,0 +1,29 @@
+package deors.demos.microservices.bookrecedgeservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+public class BookController {
+
+    @RestController
+    class QuoteController {
+        @Autowired
+        RestTemplate restTemplate;
+
+        @Value("${defaultBook}")
+        private String defaultBook;
+
+        @RequestMapping("/bookrecedge")
+        @com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand(fallbackMethod = "getDefaultBook")
+        public String getBookRecommendation() {
+            return restTemplate.getForObject("http://bookrec-service/bookrec", String.class);
+        }
+
+        public String getDefaultBook() {
+            return defaultBook;
+        }
+    }
+}
